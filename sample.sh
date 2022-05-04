c94c809b77b8b16ca0cc740c6abc136514cdf530
bluecollar-coder <anishkumarmalik@gmail.com>
2022-05-04 14:40:07 -0400

    new changes

--- sample.sh
+++ sample.sh
@@ -9,16 +9,16 @@ a=20
 a=25
 echo $a
 b=[4][12][37]
-echo $b[1]
-c=[10 20 30]
+echo ${b[2]}
+c=(10 20 30)
 echo ${c[1]}
 echo first argument is $1 and second argument is $2
 echo all arguments are $*
 echo total number of arguments is $#
 
-DATE = $(date +%F)
+DATE=$(date +%F)
 
 echo Current Date is $DATE
 
-SUM = $((2+3))
+SUM=$((2+3))
 echo Sum of Numbers 2and 3 is $SUM
